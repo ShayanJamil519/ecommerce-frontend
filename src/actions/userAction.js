71a66741af0616d7d4c4c4d61033c2bef613703f
ShayanJamil519 <shayanjamil500@gmail.com>
2023-06-19 20:29:30 +0500

    Netlify.toml File Added

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -41,7 +41,10 @@ import axios from "axios";
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: LOGIN_REQUEST });
-    const config = { headers: { "Content-Type": "application/json" } };
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    };
     const { data } = await axios.post(
       `/api/v1/login`,
       { email, password },
@@ -63,7 +66,10 @@ export const login = (email, password) => async (dispatch) => {
 export const register = (userData) => async (dispatch) => {
   try {
     dispatch({ type: REGISTER_USER_REQUEST });
-    const config = { headers: { "Content-Type": "multipart/form-data" } };
+    const config = {
+      headers: { "Content-Type": "multipart/form-data" },
+      credentials: "include",
+    };
     const { data } = await axios.post(
       `https://bewildered-ruby-meerkat.cyclic.app/api/v1/register`,
       userData,
